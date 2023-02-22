3cb11f9ad02c1af3914f5c42ee316780cfd94456
UdataGayatri <87421637+UdataGayatri@users.noreply.github.com>
2023-02-22 14:56:01 +0530

    Add files via upload

---
+++ add.rb
@@ -0,0 +1,8 @@
+class Sum
+  def add(x,y)
+    @res=x+y
+    puts  @res
+  end
+end
+obj=Sum.new
+obj.add(10,5)
---
+++ hw.rb
@@ -0,0 +1,7 @@
+class H
+  def h
+    puts "Hello Everyone!!"
+  end
+end
+n=H.new
+n.h
